887f2969c3b01beffedb4e255a7df779fc542f04
brian <90752841+wokbjso@users.noreply.github.com>
2023-09-18 22:43:27 +0900

    Feat:GlobalStyle 로 reset-css 설정 및 theme 파일 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-	return (
-		<div>
-			<h1>18기 프론트 화이팅~ 푸하항ㅋ</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>18기 프론트 화이팅~ 푸하항ㅋ</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
