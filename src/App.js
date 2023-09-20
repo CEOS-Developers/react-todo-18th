f59ceae5aa2ccde963a9224376daab93ee22788f
dhsin98 <shinjay417@gmail.com>
2023-09-21 01:11:38 +0900

    [feat]: 이동 버튼 & done 리스트 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]); //todo list를 추가할 배열
+  const [done, setDone] = useState("");
+  const [dones, setDones] = useState([]);
+
   const onChange = (event) => setTodo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
@@ -15,14 +18,33 @@ function App() {
     //기존 todos 에서 입력받은 todo 추가해서 new array 저장
     setTodo("");
   };
-  const deleteBtn = (index) => {
-    setTodos(todos.filter((item, todoIndex) => index !== todoIndex));
+  //삭제 버튼
+  const deleteBtn = (index, isDone) => {
+    if (isDone === true) {
+      setDones(dones.filter((item, doneIndex) => index !== doneIndex));
+    } else {
+      setTodos(todos.filter((item, todoIndex) => index !== todoIndex));
+    }
+  };
+
+  const moveBtn = (index, isDone) => {
+    if (isDone === false) {
+      //todos-> dones
+      const itemToMove = todos[index];
+      setTodos(todos.filter((item, todoIndex) => index !== todoIndex)); //todos 에서 제거
+      setDones((currentArray) => [itemToMove, ...currentArray]); //dones으로 이동
+    } else {
+      //dones -> todos
+      const itemToMove = dones[index];
+      setDones(dones.filter((item, doneIndex) => index !== doneIndex)); // dones 제거
+      setTodos((currentArray) => [itemToMove, ...currentArray]); //todos으로 이동
+    }
   };
 
   return (
     <div>
       <h1>To-Do List</h1>
-      <h2>To Do : {todos.length}</h2>
+
       <form onSubmit={onSubmit}>
         <input
           type="text"
@@ -33,11 +55,24 @@ function App() {
         <button>Add to Do</button>
       </form>
       <hr />
+      <h2>To Do : {todos.length}</h2>
       <ul>
         {todos.map((item, index) => (
           <li key={index}>
             {item}
-            <button onClick={() => deleteBtn(index)}>❌</button>
+            <button onClick={() => moveBtn(index, false)}>↕️</button>
+            <button onClick={() => deleteBtn(index, false)}>❌</button>
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <h2> Done : {dones.length}</h2>
+      <ul>
+        {dones.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => moveBtn(index, true)}>↕️</button>
+            <button onClick={() => deleteBtn(index, true)}>❌</button>
           </li>
         ))}
       </ul>
