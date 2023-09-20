87c055f1c68888f1acb2c70b3ce9f85eb433cbe5
김지원 <jibom0507@naver.com>
2023-09-21 03:12:16 +0900

    feat : clock.js

--- src/Clock.js
+++ src/Clock.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
+
 const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  const timeOptions = {
+    hour12: false, // 24 시간 형식 사용
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
   return (
     <div className="Clock">
-      <h1>시계</h1>
+      <h1>{time.toLocaleTimeString([], timeOptions)}</h1>
     </div>
   );
 };
