3d274f86f76c900cf81c9a273a65410945b432d6
Jiseok <chae0_@naver.com>
2023-09-21 21:32:43 +0900

    [feat]switch,delete,addTodo 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import React, { useState, useEffect } from "react";
+import TodoList from "./components/todoList";
+import DoneList from "./components/doneList";
+import InputTodo from "./components/inputTodo";
+
 function App() {
-	return (
-		<div>
-			<h1>18기 프론트 화이팅~ 푸하항ㅋ</h1>
-		</div>
-	);
+  const [todos, setTodos] = useState([]);
+  const [dones, setDones] = useState([]);
+
+  const addTodo = (item) => {
+    setTodos([...todos, { id: new Date().getTime(), text: item }]);
+  };//item 추가
+
+  const deleteItem = (listName, index) => {
+    const list = listName === "todos" ? todos : dones;
+    const newList = [...list];
+    newList.splice(index, 1);
+    listName === "todos" ? setTodos(newList) : setDones(newList);
+  };
+
+//기존의 리스트와, 인덱스를 받아 todo <-> done , 
+
+  const switchList = (fromListName, index) => {
+    let fromList, toList, setFromList, setToList;
+    if (fromListName === "todos") {
+      fromList = todos;
+      toList = dones;
+      setFromList = setTodos;
+      setToList = setDones;
+    } else {
+      fromList = dones;
+      toList = todos;
+      setFromList = setDones;
+      setToList = setTodos;
+    }
+
+    const itemToMove = fromList[index];
+    const newFromList = [...fromList];
+    newFromList.splice(index, 1);
+    setFromList(newFromList);
+
+    const newToList = [...toList, itemToMove];
+    setToList(newToList);
+  };
+
+  return (
+    <>
+      <main>
+        <h1>할 일</h1>
+        <InputTodo addTodo={addTodo} />
+        <TodoList todos={todos} switchList={switchList} deleteItem={deleteItem} />
+        <DoneList dones={dones} switchList={switchList} deleteItem={deleteItem} />
+      </main>
+    </>
+  );
 }
 
 export default App;
